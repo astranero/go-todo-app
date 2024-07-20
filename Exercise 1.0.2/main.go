9cfc01a65091f70b34efc7ab818895d25847b051
astranero <esa.taheresa@gmail.com>
2024-07-20 14:54:10 +0300

    run logoutput

--- Exercise 1.0.2/main.go
+++ Exercise 1.0.2/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"fmt"
 	"log"
@@ -8,6 +10,8 @@ import (
 )
 
 func main(){
+	router := gin.Default()
+
 	randomString := uuid.New().String()
 	log.Println("Application Started")
 
@@ -15,4 +19,14 @@ func main(){
 	for t := range ticker.C {
 		fmt.Printf("%s: %s\n", t.Format(time.RFC3339), randomString)
 	}
+
+	InitDatabase()
+
+	router.GET("/todos", GetTodos)
+	router.POST("/todos", PostTodo)
+	router.GET("/todos/:id", GetTodoByID)
+	router.PUT("/todos/", UpdateTodo)
+	router.DELETE("/todos/:id", DeleteTodo)
+
+	router.Run(":8080")
 }
